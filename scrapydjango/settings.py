2953bbd1027d9659eb74613a73bf127234b4907d
vishal <hiringnext1@gmail.com>
2019-06-18 23:44:48 +0530

    Make it Better

--- scrapydjango/settings.py
+++ scrapydjango/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,8 +131,7 @@ STATICFILES_DIRS = [
     os.path.join(os.path.join(BASE_DIR, "static", ))
     ]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static_root', )
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root', )
 
 
 MEDIA_URL = '/media/'
@@ -141,7 +141,13 @@ MEDIAFILES_DIRS = [
     os.path.join(os.path.join(BASE_DIR, "media", ))
     ]
 
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root', )
 
 
 
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+import dj_database_url
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
